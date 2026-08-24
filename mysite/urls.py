15aaa84cff354a4c36cf95c4dfa1fbdb1f8fa4e1
John Njoroge <john.mnjoroge456@gmail.com>
2026-08-24 16:30:18 +0300

    Add Bootstrap layout and logo to base.html; enable dev media serving

--- mysite/urls.py
+++ mysite/urls.py
@@ -5,3 +5,10 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('main.urls')),
 ]
+# --- dev static/media serving (appended by assistant) ---
+from django.conf import settings
+from django.conf.urls.static import static
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# --- end appended block ---
